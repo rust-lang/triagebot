482a12d101d8b3b2870f2a774fefe732bd0dbe2a
Mark Rousskov <mark.simulacrum@gmail.com>
2019-02-19 17:30:48 -0700

    Disable assignment and tracking issue handlers
    Neither is ready for prime time yet.

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,18 +1,18 @@
 use crate::github::GithubClient;
 use crate::registry::HandleRegistry;
 
-mod assign;
+//mod assign;
 mod label;
-mod tracking_issue;
+//mod tracking_issue;
 
 pub fn register_all(registry: &mut HandleRegistry, client: GithubClient) {
     registry.register(label::LabelHandler {
         client: client.clone(),
     });
-    registry.register(assign::AssignmentHandler {
-        client: client.clone(),
-    });
-    registry.register(tracking_issue::TrackingIssueHandler {
-        client: client.clone(),
-    });
+    //registry.register(assign::AssignmentHandler {
+    //    client: client.clone(),
+    //});
+    //registry.register(tracking_issue::TrackingIssueHandler {
+    //    client: client.clone(),
+    //});
 }
