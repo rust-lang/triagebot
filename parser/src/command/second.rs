45abaf2b550b26c30916222cb2002b10be97b3dc
Mark Rousskov <mark.simulacrum@gmail.com>
2020-04-13 17:51:55 -0400

    Permit `seconded` as well as `second`

--- parser/src/command/second.rs
+++ parser/src/command/second.rs
@@ -8,6 +8,8 @@ impl SecondCommand {
     pub fn parse<'a>(input: &mut Tokenizer<'a>) -> Result<Option<Self>, Error<'a>> {
         if let Some(Token::Word("second")) = input.peek_token()? {
             Ok(Some(Self))
+        } else if let Some(Token::Word("seconded")) = input.peek_token()? {
+            Ok(Some(Self))
         } else {
             Ok(None)
         }
