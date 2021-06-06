c0b0eeb748aa9883981a034010a2ef563a146ced
mibac138 <5672750+mibac138@users.noreply.github.com>
2021-06-06 17:50:12 +0200

    Add review_submitted handler which changes labels after a review
    When an assigned reviewer submits a review to a PR, automatically
    remove waiting-on-review and add waiting-on-author label

---
+++ src/handlers/review_submitted.rs
@@ -0,0 +1,42 @@
+use crate::github::{Issue, IssueCommentAction, IssueCommentEvent, Label};
+use crate::{config::ReviewSubmittedConfig, github::Event, handlers::Context};
+
+pub(crate) async fn handle(
+    ctx: &Context,
+    event: &Event,
+    config: &ReviewSubmittedConfig,
+) -> anyhow::Result<()> {
+    if let Event::IssueComment(
+        event
+        @
+        IssueCommentEvent {
+            action: IssueCommentAction::Created,
+            issue: Issue {
+                pull_request: Some(_),
+                ..
+            },
+            ..
+        },
+    ) = event
+    {
+        if event.issue.assignees.contains(&event.comment.user) {
+            let labels = event
+                .issue
+                .labels()
+                .iter()
+                // Remove review related labels
+                .filter(|label| !config.review_labels.contains(&label.name))
+                .cloned()
+                // Add waiting on author label
+                .chain(std::iter::once(Label {
+                    name: config.reviewed_label.clone(),
+                }));
+            event
+                .issue
+                .set_labels(&ctx.github, labels.collect())
+                .await?;
+        }
+    }
+
+    Ok(())
+}
