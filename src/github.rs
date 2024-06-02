e5e4c928d20654daebe0c81875807ca1de9c25ac
Eric Huss <eric@huss.org>
2024-06-01 17:41:22 -0700

    Disable GitHub retry on the server.
    This disables the GitHub rate limit retry logic on the server. Generally
    for webhooks, the server must respond within 10 seconds. If it ever
    hits the rate limit, it will likely sleep for several minutes, meaning
    it will be guaranteed to be killed.
    
    Whether or not retries are enabled is now a property of `GithubClient`.
    It is enabled for the CLI prioritization tool (which added this retry
    logic in the first place in
    https://github.com/rust-lang/triagebot/pull/579).

--- src/github.rs
+++ src/github.rs
@@ -31,8 +31,10 @@ impl GithubClient {
             .with_context(|| format!("building reqwest {}", req_dbg))?;
 
         let mut resp = self.client.execute(req.try_clone().unwrap()).await?;
-        if let Some(sleep) = Self::needs_retry(&resp).await {
-            resp = self.retry(req, sleep, MAX_ATTEMPTS).await?;
+        if self.retry_rate_limit {
+            if let Some(sleep) = Self::needs_retry(&resp).await {
+                resp = self.retry(req, sleep, MAX_ATTEMPTS).await?;
+            }
         }
         let maybe_err = resp.error_for_status_ref().err();
         let body = resp
@@ -2146,6 +2148,8 @@ pub struct GithubClient {
     api_url: String,
     graphql_url: String,
     raw_url: String,
+    /// If `true`, requests will sleep if it hits GitHub's rate limit.
+    retry_rate_limit: bool,
 }
 
 impl GithubClient {
@@ -2156,6 +2160,7 @@ impl GithubClient {
             api_url,
             graphql_url,
             raw_url,
+            retry_rate_limit: false,
         }
     }
 
@@ -2169,6 +2174,14 @@ impl GithubClient {
             std::env::var("GITHUB_RAW_URL")
                 .unwrap_or_else(|_| "https://raw.githubusercontent.com".to_string()),
         )
+    }
+
+    /// Sets whether or not this client will retry when it hits GitHub's rate limit.
+    ///
+    /// Just beware that the retry may take a long time (like 30 minutes,
+    /// depending on various factors).
+    pub fn set_retry_rate_limit(&mut self, retry: bool) {
+        self.retry_rate_limit = retry;
     }
 
     pub fn raw(&self) -> &Client {
