3d156cbc35ec006046bebdb7c4ce8e6c5a8b6f46
apiraino <apiraino@users.noreply.github.com>
2025-03-11 17:34:33 +0100

    Add ReviewPrefs upsert query

--- src/db/review_prefs.rs
+++ src/db/review_prefs.rs
@@ -1,4 +1,5 @@
-use crate::github::UserId;
+use crate::db::users::record_username;
+use crate::github::{User, UserId};
 use anyhow::Context;
 use serde::Serialize;
 
@@ -34,4 +35,95 @@ WHERE review_prefs.user_id = $1;";
         .await
         .context("Error retrieving review preferences")?;
     Ok(row.map(|r| r.into()))
+}
+
+/// Updates review preferences of the specified user, or creates them
+/// if they do not exist yet.
+pub async fn upsert_review_prefs(
+    db: &tokio_postgres::Client,
+    user: User,
+    max_assigned_prs: Option<u32>,
+) -> anyhow::Result<u64, anyhow::Error> {
+    // We need to have the user stored in the DB to have a valid FK link in review_prefs
+    record_username(db, user.id, &user.login).await?;
+
+    let max_assigned_prs = max_assigned_prs.map(|v| v as i32);
+    let query = "
+INSERT INTO review_prefs(user_id, max_assigned_prs)
+VALUES ($1, $2)
+ON CONFLICT (user_id)
+DO UPDATE
+SET max_assigned_prs = excluded.max_assigned_prs";
+
+    let res = db
+        .execute(query, &[&(user.id as i64), &max_assigned_prs])
+        .await
+        .context("Error upserting review preferences")?;
+    Ok(res)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::db::review_prefs::{get_review_prefs, upsert_review_prefs};
+    use crate::db::users::get_user;
+    use crate::tests::github::user;
+    use crate::tests::run_test;
+
+    #[tokio::test]
+    async fn insert_prefs_create_user() {
+        run_test(|ctx| async {
+            let db = ctx.db_client().await;
+
+            let user = user("Martin", 1);
+            upsert_review_prefs(&db, user.clone(), Some(1)).await?;
+
+            assert_eq!(get_user(&db, user.id).await?.unwrap(), user);
+
+            Ok(ctx)
+        })
+        .await;
+    }
+
+    #[tokio::test]
+    async fn insert_max_assigned_prs() {
+        run_test(|ctx| async {
+            let db = ctx.db_client().await;
+
+            upsert_review_prefs(&db, user("Martin", 1), Some(5)).await?;
+            assert_eq!(
+                get_review_prefs(&db, 1).await?.unwrap().max_assigned_prs,
+                Some(5)
+            );
+
+            Ok(ctx)
+        })
+        .await;
+    }
+
+    #[tokio::test]
+    async fn update_max_assigned_prs() {
+        run_test(|ctx| async {
+            let db = ctx.db_client().await;
+
+            upsert_review_prefs(&db, user("Martin", 1), Some(5)).await?;
+            assert_eq!(
+                get_review_prefs(&db, 1).await?.unwrap().max_assigned_prs,
+                Some(5)
+            );
+            upsert_review_prefs(&db, user("Martin", 1), Some(10)).await?;
+            assert_eq!(
+                get_review_prefs(&db, 1).await?.unwrap().max_assigned_prs,
+                Some(10)
+            );
+
+            upsert_review_prefs(&db, user("Martin", 1), None).await?;
+            assert_eq!(
+                get_review_prefs(&db, 1).await?.unwrap().max_assigned_prs,
+                None
+            );
+
+            Ok(ctx)
+        })
+        .await;
+    }
 }
