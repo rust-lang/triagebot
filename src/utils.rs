b19b0cac043e094723d941333b56c8876029157f
Urgau <urgau@numericable.fr>
2025-08-02 17:20:44 +0200

    Add AppError struct to convert anyhow::Error to axum Response

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,7 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 use std::borrow::Cow;
 
 /// Pluralize (add an 's' sufix) to `text` based on `count`.
@@ -6,5 +10,27 @@ pub fn pluralize(text: &str, count: usize) -> Cow<'_, str> {
         text.into()
     } else {
         format!("{}s", text).into()
+    }
+}
+
+pub struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        tracing::error!("error={}", &self.0);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        AppError(err.into())
     }
 }
