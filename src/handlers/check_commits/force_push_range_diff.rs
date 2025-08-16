caf4f50c171e588e3e1a41223c4233bf77d8a363
Urgau <urgau@numericable.fr>
2025-08-16 17:04:12 +0200

    Hide previous range-diff comment

--- src/handlers/check_commits/force_push_range_diff.rs
+++ src/handlers/check_commits/force_push_range_diff.rs
@@ -1,10 +1,22 @@
 use anyhow::Context as _;
 
 use crate::config::RangeDiffConfig;
+use crate::db::issue_data::IssueData;
 use crate::github::GithubCompare;
 use crate::github::IssueRepository;
 use crate::github::IssuesEvent;
+use crate::github::ReportedContentClassifiers;
 use crate::handlers::Context;
+
+/// Key for the state in the database
+const RANGE_DIFF_LINK_KEY: &str = "range-diff-link";
+
+/// State stored in the database
+#[derive(Debug, Default, serde::Deserialize, serde::Serialize, Clone, PartialEq)]
+struct RangeDiffLinkState {
+    /// ID of the most recent range-diff comment.
+    last_comment: Option<String>,
+}
 
 pub(super) async fn handle_event(
     ctx: &Context,
@@ -54,10 +66,49 @@ pub(super) async fn handle_event(
     let (oldbase, oldhead) = (&compare_before.merge_base_commit.sha, before);
     let (newbase, newhead) = (&compare_after.merge_base_commit.sha, after);
 
-    // Rebase detected, post a comment to our range-diff.
-    event.issue.post_comment(&ctx.github,
-        &format!(r#"This PR was rebased onto a different {branch} commit! Check out the changes with our [`range-diff`]({protocol}://{host}/gh-range-diff/{org}/{repo}/{oldbase}..{oldhead}/{newbase}..{newhead})."#)
-    ).await.context("failed to post range-diff comment")?;
+    let message = format!(
+        r#"This PR was rebased onto a different {branch} commit! Check out the changes with our [`range-diff`]({protocol}://{host}/gh-range-diff/{org}/{repo}/{oldbase}..{oldhead}/{newbase}..{newhead})."#
+    );
+
+    // Rebase detected, post a comment linking to our range-diff.
+    post_new_comment(ctx, event, message).await?;
+
+    Ok(())
+}
+
+async fn post_new_comment(
+    ctx: &Context,
+    event: &IssuesEvent,
+    message: String,
+) -> anyhow::Result<()> {
+    let mut db = ctx.db.get().await;
+    let mut state: IssueData<'_, RangeDiffLinkState> =
+        IssueData::load(&mut db, &event.issue, RANGE_DIFF_LINK_KEY).await?;
+
+    // Hide previous range-diff comment.
+    if let Some(last_comment) = state.data.last_comment {
+        event
+            .issue
+            .hide_comment(
+                &ctx.github,
+                &last_comment,
+                ReportedContentClassifiers::Outdated,
+            )
+            .await
+            .context("failed to hide previous range-diff comment")?;
+    }
+
+    // Post new range-diff comment and remember it.
+    state.data.last_comment = Some(
+        event
+            .issue
+            .post_comment(&ctx.github, &message)
+            .await
+            .context("failed to post range-diff comment")?
+            .node_id,
+    );
+
+    state.save().await?;
 
     Ok(())
 }
