d978fc58eec98523ccd8246d3444f8a8a534ca12
Ada Alakbarova <ada.alakbarova@proton.me>
2025-09-15 19:23:26 +0200

    reborrow instead of slicing

--- src/bin/compiler.rs
+++ src/bin/compiler.rs
@@ -7,7 +7,7 @@ async fn main() -> anyhow::Result<()> {
 
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 2 {
-        match &args[1][..] {
+        match &*args[1] {
             "backlog_bonanza" => {
                 let agenda = agenda::compiler_backlog_bonanza();
                 print!("{}", agenda.call().await?);
