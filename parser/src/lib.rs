167fdcbdc19f7c32773886e5e3720ade4000db30
apiraino <apiraino@users.noreply.github.com>
2025-10-17 18:34:04 +0200

    fixup

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -2,8 +2,7 @@ pub mod command;
 pub mod error;
 mod ignore_block;
 mod mentions;
-// XXX: is this necessary?
-pub mod token;
+mod token;
 
 pub use ignore_block::replace_all_outside_ignore_blocks;
 pub use mentions::get_mentions;
