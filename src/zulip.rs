8d255be42cbd21fd7232b59e31a2f18c07afa167
Urgau <urgau@numericable.fr>
2026-03-07 20:56:58 +0100

    Replace hand-written markdown normalization with proper stripping

--- src/zulip.rs
+++ src/zulip.rs
@@ -1658,14 +1658,7 @@ fn format_date(date: Option<DateTime<Utc>>) -> String {
 /// Truncates the given text to the specified length, adding ellipsis if needed.
 /// Also removes various special Markdown symbols from it.
 fn truncate_and_normalize(text: &str, max_len: usize) -> String {
-    let normalized: String = text
-        .split_whitespace()
-        .collect::<Vec<_>>()
-        .join(" ")
-        // Avoid things that could interact with the rest of the Markdown message.
-        // And avoid and @ pings (in case the text is outputted in a public Zulip stream).
-        .replace("`", "")
-        .replace("@", "");
+    let normalized = parser::strip_markdown(text);
 
     if normalized.len() <= max_len {
         normalized
