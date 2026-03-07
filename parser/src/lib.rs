de4b91126cde7f1f5d9a0d15f1feabda75001d3d
Urgau <urgau@numericable.fr>
2026-03-07 20:56:24 +0100

    Add routine to strip any markdown

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -2,7 +2,9 @@ pub mod command;
 pub mod error;
 mod ignore_block;
 mod mentions;
+mod text;
 mod token;
 
 pub use ignore_block::replace_all_outside_ignore_blocks;
 pub use mentions::get_mentions;
+pub use text::strip_markdown;
