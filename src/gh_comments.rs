1cc562c48b46adf4d6c90c320d00564045c56a35
Urgau <urgau@numericable.fr>
2026-05-29 19:03:50 +0200

    Make the cursor style reflect that it can be clicked on hidden comment

--- src/gh_comments.rs
+++ src/gh_comments.rs
@@ -29,7 +29,7 @@ use crate::{
     utils::{immutable_headers, is_known_and_public_repo},
 };
 
-pub const STYLE_URL: &str = "/gh-comments/style@0.0.8.css";
+pub const STYLE_URL: &str = "/gh-comments/style@0.0.9.css";
 pub const MARKDOWN_URL: &str = "/gh-comments/github-markdown@20260117.css";
 pub const SELF_CONTAINED_URL: &str = "/gh-comments/self_contained@0.0.3.js";
 pub const RELATIVE_TIME_ELEMENT_URL: &str = "/gh-comments/relative-time-element@5.0.0.js";
